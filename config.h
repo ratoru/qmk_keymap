958ffe74e9ae91d39abb951787a6f8f23b2abdd4
Raphael <raphael.t.ruban@gmail.com>
2023-07-18 23:51:22 +0200

    try config.h

--- config.h
+++ config.h
@@ -1,1 +1,6 @@
 #define LAYER_STATE_8BIT // Limit the number of layers to 8 to save firmware size.
+
+// custom keycodes
+#define SELWORD QK_USER
+#define UPDIR QK_USER + 1
+#define LLOCK QK_USER + 2
