c793efb9020f6bc654e33739cbd44161a8f3e542
Raphael <raphael.t.ruban@gmail.com>
2023-07-20 00:37:51 +0200

    move #define

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 #pragma once
-// #include "quantum.h"
 
 #define LAYER_STATE_8BIT // Limit the number of layers to 8 to save firmware size.
 
 #ifndef __ASSEMBLER__
+#include "quantum.h"
 // Define all of custom keycodes. Needs to be protected.
 enum userspace_custom_keycodes {
   UPDIR = SAFE_RANGE,
