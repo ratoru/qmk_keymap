82efdc7e9f585f26c1590bb23b23e5936e5a8cfc
Raphael <raphael.t.ruban@gmail.com>
2023-07-20 00:55:59 +0200

    reduce size

--- rules.mk
+++ rules.mk
@@ -2,17 +2,18 @@ SRC += source.c
 SRC += features/select_word.c
 SRC += features/layer_lock.c
 
+# Enable used features 
 CAPS_WORD_ENABLE = yes
 REPEAT_KEY_ENABLE = yes
 
 # Reduce size
 LTO_ENABLE = yes
-# CONSOLE_ENABLE = no
-# COMMAND_ENABLE = no
-# MOUSEKEY_ENABLE = no
-# SPACE_CADET_ENABLE = no
-# GRAVE_ESC_ENABLE = no 
-# MAGIC_ENABLE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no 
+MAGIC_ENABLE = no
 
-# look here for custom source codes while compiling
-# INTROSPECTION_KEYMAP_C = ratoru.c 
+# look here for layout while compiling (not needed)
+# INTROSPECTION_KEYMAP_C = source.c 
