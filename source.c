0e4d65b90df00e368f72cd0bb58aeea51fd35e16
Raphael <raphael.t.ruban@gmail.com>
2024-12-07 08:40:40 -0800

    chore: cleanup comments

--- source.c
+++ source.c
@@ -105,6 +105,66 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
   return true;
 }
 
+// ---- Home Row Mods "Timeless" Config ----
+#if defined (PERMISSIVE_HOLD_PER_KEY) || defined (HOLD_ON_OTHER_KEY_PRESS_PER_KEY) || defined (TAP_INTERVAL_MS)
+static uint16_t    next_keycode;
+static keyrecord_t next_record;
+
+bool pre_process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // Instant Tap Config
+    static uint16_t prev_keycode;
+    if (record->event.pressed) {
+        // Store the previous keycode for instant tap decision
+        prev_keycode = next_keycode;
+        // Cache the next input for mod-tap decisions
+        next_keycode = keycode;
+        next_record  = *record;
+    }
+    // Match home row mod-tap keys when it is not preceded by a Layer key
+    if (IS_HOMEROW(record) && IS_QK_MOD_TAP(keycode) && !IS_QK_LAYER_TAP(prev_keycode)) {
+        // Tap the mod-tap key instantly when it follows a short interval
+        if (record->event.pressed && last_input_activity_elapsed() < TAP_INTERVAL_MS) {
+            record->keycode = keycode & 0xff;
+            action_tapping_process(*record);
+            return false;
+        } else { // Send the base keycode key up event
+            keyrecord_t base_record   = *record;
+            base_record.keycode       = keycode & 0xff;
+            base_record.event.pressed = false;
+            action_tapping_process(base_record);
+        }
+    }
+    return true;
+}
+#endif
+
+#ifdef PERMISSIVE_HOLD_PER_KEY
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+    // Hold Control and Shift with a nested key tap on the opposite hand
+    return IS_BILATERAL_TAP(record, next_record);
+}
+#endif
+
+
+#ifdef HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    // Replace the mod-tap key with its base keycode when
+    // tapped with another key on the same hand
+    if (IS_UNILATERAL_TAP(record, next_record)) {
+        record->keycode = keycode & 0xff;
+        process_record(record);
+        record->event.pressed = false;
+        process_record(record);
+        return true;
+    }
+    // Hold layer with another key press
+    else if (IS_QK_LAYER_TAP(keycode) && QK_LAYER_TAP_GET_LAYER(keycode)) {
+        return true;
+    }
+    return false;
+}
+#endif
+
 #ifdef RGBLIGHT_ENABLE
 void keyboard_pre_init_user(void) {
   // Set our LED pin as output
--- source.h
+++ source.h
@@ -1,17 +1,17 @@
 #pragma once
 #include QMK_KEYBOARD_H
 
-/*#ifdef TAPPING_TERM*/
-/*#define IS_HOMEROW(r)        (r->event.key.row == 1 || r->event.key.row == 5)*/
-/**/
-/*#define IS_UNILATERAL_TAP(r, n) ((r->event.key.col != n.event.key.col) && (    \*/
-/*    (r->event.key.row == 1 && 0 <= n.event.key.row && n.event.key.row <= 2) || \*/
-/*    (r->event.key.row == 5 && 4 <= n.event.key.row && n.event.key.row <= 6) ))*/
-/**/
-/*#define IS_BILATERAL_TAP(r, n) ( \*/
-/*    (r->event.key.row == 1 && 4 <= n.event.key.row && n.event.key.row <= 6) || \*/
-/*    (r->event.key.row == 5 && 0 <= n.event.key.row && n.event.key.row <= 2) )*/
-/*#endif*/
+#ifdef TAPPING_TERM
+#define IS_HOMEROW(r)        (r->event.key.row == 1 || r->event.key.row == 5)
+
+#define IS_UNILATERAL_TAP(r, n) ((r->event.key.col != n.event.key.col) && (    \
+    (r->event.key.row == 1 && 0 <= n.event.key.row && n.event.key.row <= 2) || \
+    (r->event.key.row == 5 && 4 <= n.event.key.row && n.event.key.row <= 6) ))
+
+#define IS_BILATERAL_TAP(r, n) ( \
+    (r->event.key.row == 1 && 4 <= n.event.key.row && n.event.key.row <= 6) || \
+    (r->event.key.row == 5 && 0 <= n.event.key.row && n.event.key.row <= 2) )
+#endif
 
 #ifdef COMBO_ENABLE
 #include "features/combos.h"
